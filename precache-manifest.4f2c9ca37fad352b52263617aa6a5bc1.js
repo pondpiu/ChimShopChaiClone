33d3f25d30bb1d4c20b9a3bd63291c54fc3b0106
pondpiu <peerawas.a@gmail.com>
2019-10-29 21:35:38 +0700

    Updates

---
+++ precache-manifest.4f2c9ca37fad352b52263617aa6a5bc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cee02c6ba6daced113d9766f2cd163ab",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "81487d33040f3f7eeba9",
+    "url": "/react-gh-pages/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e1633bb6c9a900f349e3",
+    "url": "/react-gh-pages/static/js/2.4f3d2838.chunk.js"
+  },
+  {
+    "revision": "81487d33040f3f7eeba9",
+    "url": "/react-gh-pages/static/js/main.33c27374.chunk.js"
+  },
+  {
+    "revision": "880669e599c0801ea816",
+    "url": "/react-gh-pages/static/js/runtime-main.566348f1.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-gh-pages/static/media/logo.25bf045c.svg"
+  }
+]);
